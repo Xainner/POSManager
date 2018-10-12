da35634af0fbf62fb3161d2216ecfd29b1dc9b8b
Saul Reyes <sauldf93@hotmail.com>
2018-10-12 14:23:25 -0600

    Ventana productos funcionando

--- POSManager/LogicLibrary/Management/ProductManagement.cs
+++ POSManager/LogicLibrary/Management/ProductManagement.cs
@@ -10,6 +10,59 @@ namespace LogicLibrary.Management
 {
     public static class ProductManagement
     {
+        public static bool InsertProduct(
+            string code,
+            string style,
+            string idBrand,
+            string description,
+            string idSubCategory,
+            string normalPrice,
+            string lowerPrice,
+            string estableQuantity,
+            string variableQuantity,
+            byte[] image,
+            bool ivi,
+            bool existingInvoice
+        )
+        {
+            try
+            {
+                string[] Product = new string[] {
+                    code,
+                    style,
+                    idBrand,
+                    description,
+                    idSubCategory,
+                    normalPrice,
+                    lowerPrice,
+                    estableQuantity,
+                    variableQuantity
+                };
+                ProductModel productModel = new ProductModel()
+                {
+                    Code = code,
+                    Style = style,
+                    idBrand = int.Parse(idBrand),
+                    Description = description,
+                    idsubCategory = int.Parse(idSubCategory),
+                    normalPrice = decimal.Parse(normalPrice),
+                    lowerPrice = decimal.Parse(lowerPrice),
+                    estableQuantity = int.Parse(estableQuantity),
+                    variableQuantity = int.Parse(variableQuantity),
+                    Ivi = ivi,
+                    existingInvoice = existingInvoice,
+                    Image = image
+                };
+
+                return ProductConnection.InsertProduct(productModel);
+            }
+            catch (Exception ex)
+            {
+                //Log4Net
+                return false;
+            }
+        }
+
         public static ProductModel SelectProductByCode(string code)
         {
             try
@@ -24,6 +77,91 @@ namespace LogicLibrary.Management
             {
 
                 throw;
+            }
+        }
+
+        public static bool UpdateProductById(
+            string idProduct,
+            string code, string style,
+            string idBrand,
+            string description,
+            string idSubCategory,
+            string normalPrice,
+            string lowerPrice,
+            string estableQuantity,
+            string variableQuantity,
+            byte[] image,
+            bool ivi,
+            bool existingInvoice
+        )
+        {
+            try
+            {
+                string[] product = new string[] {
+                    idProduct,
+                    code,  style,
+                    idBrand,
+                    description,
+                    idSubCategory,
+                    normalPrice,
+                    lowerPrice,
+                    estableQuantity,
+                    variableQuantity
+                };
+                    ProductModel productModel = new ProductModel()
+                    {
+                        idProduct = int.Parse(idProduct),
+                        Code = code,
+                        Style = style,
+                        idBrand = int.Parse(idBrand),
+                        Description = description,
+                        idsubCategory = int.Parse(idSubCategory),
+                        normalPrice = decimal.Parse(normalPrice),
+                        lowerPrice = decimal.Parse(lowerPrice),
+                        estableQuantity = int.Parse(estableQuantity),
+                        variableQuantity = int.Parse(variableQuantity),
+                        Ivi = ivi,
+                        existingInvoice = existingInvoice,
+                        Image = image
+
+                    };
+                    return ProductConnection.UpdateProductById(productModel);
+            }
+            catch (Exception ex)
+            {
+                //Log4Net
+                return false;
+            }
+        }
+
+        public static bool DeleteProductById(string idProduct)
+        {
+            try
+            {
+                string[] product = new string[] { idProduct };
+                ProductModel productModel = new ProductModel()
+                {
+                    idProduct = int.Parse(idProduct),
+                };
+                return ProductConnection.DeleteProductById(productModel);
+            }
+            catch (Exception ex)
+            {
+                //Log4Net
+                return false;
+            }
+        }
+
+        public static List<ProductModel> SelectAllProducts()
+        {
+            try
+            {
+                return ProductConnection.SelectAllProducts();
+            }
+            catch (Exception ex)
+            {
+                //Log4Net
+                return null;
             }
         }
     }
